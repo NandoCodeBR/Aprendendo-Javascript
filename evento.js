3687242682202433ed91c8b33fa893f27e3506e4
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2019-11-25 20:24:08 -0300

    Update evento.js

--- evento.js
+++ evento.js
@@ -1,3 +1,7 @@
 function mensagem(nome){
     alert("Ei, não clique em mim "+nome);
 }
+function troca(cor){
+    var elemento = document.getElementById("hello");
+    elemento.style.backgroundColor=(cor);
+}
